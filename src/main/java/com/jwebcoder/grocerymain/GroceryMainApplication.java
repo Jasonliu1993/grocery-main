2c8722296cf2a109fe22449386714f4092056861
Jason <ozil.liu2@gmail.com>
2018-07-28 20:54:08 +0800

    init

---
+++ src/main/java/com/jwebcoder/grocerymain/GroceryMainApplication.java
@@ -0,0 +1,12 @@
+package com.jwebcoder.grocerymain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryMainApplication.class, args);
+    }
+}
